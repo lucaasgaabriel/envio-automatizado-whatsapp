685b04897de79273ab5c7277239f6518de301f2b
Lucas Marques <lucaas.gaabriel@live.com>
2024-08-14 22:29:35 -0300

    Refactor: Alteração de localização de arquivos

---
+++ main.py
@@ -0,0 +1,19 @@
+import json
+import time
+from whatsapp_server import WhatsappServer
+
+api_key = "" #Aqui vai a chave de API
+host = f"http://localhost:3000"
+session_name = "" #Aqui vai o nome da sessão desejada
+
+msg = '''*TESTE AUTOMATIZADO WHATSAPP WRAPPER API*'''
+
+nums = [""] #Aqui vão os números desejados para envio da mensagem de WhatsApp
+wpp = WhatsappServer(host= host, session_name= session_name, api_key=api_key)
+
+print(wpp.start_session())
+print(wpp.get_status())
+
+for num in nums:
+    print(wpp.send_message(f'{num}',msg))
+    time.sleep(1)
