1fa45217e94685bdbe59a303838f4e641521690d
Lucas Marques <lucaas.gaabriel@live.com>
2024-08-14 22:27:20 -0300

    Refactor: update main.py para retirada de dados sensíveis

--- poder360-whatsapp-script/main.py
+++ poder360-whatsapp-script/main.py
@@ -2,33 +2,18 @@ import json
 import time
 from whatsapp_server import WhatsappServer
 
-api_key = "P360#wsd"
+api_key = "" #Aqui vai a chave de API
 host = f"http://localhost:3000"
-session_name = "poder360"
+session_name = "" #Aqui vai o nome da sessão desejada
 
-msg = '''*TESTE AUTOMATIZADO WHATSAPP WRAPPER API*
+msg = '''*TESTE AUTOMATIZADO WHATSAPP WRAPPER API*'''
 
-🌞 Oi, eis a 2ª edição do Drive/Poder360 📲 Caixa espera R$ 18 bi com bets
-
-https://poder.cc/lmoTy2
-Caso o link não esteja clicável, mande um ok ou salve o nosso número.
-
-O acesso ao Drive é via Poder Monitor.
-se você ainda não tem senha, é fácil:
-o e-mail de login é o mesmo pelo qual você recebe o Drive e a senha é: monitor@123
-
-Pronto. É um procedimento único e simples. Depois, não precisa mais repetir.
-
-*É recomendado alterar a senha depois do primeiro acesso*'''
-
-nums = ["556193008378@c.us","556183773136@c.us","556192022662@c.us", "556181512236@c.us", "556198557131@c.us"]
+nums = [""] #Aqui vão os números desejados para envio da mensagem de WhatsApp
 wpp = WhatsappServer(host= host, session_name= session_name, api_key=api_key)
 
 print(wpp.start_session())
 print(wpp.get_status())
 
-print(wpp.send_message('556183773136@c.us', msg))
-
-# for num in nums:
-#     print(wpp.send_message(f'{num}',msg))
-#     time.sleep(1)
+for num in nums:
+    print(wpp.send_message(f'{num}',msg))
+    time.sleep(1)
